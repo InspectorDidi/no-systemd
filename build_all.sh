ffa6ac0b6a679417a92f10d07cad983baddde4a7
Eduardo Trápani <etrapani@gmail.com>
2017-11-20 18:51:35 -0300

    Modified script to build all

--- build_all.sh
+++ build_all.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 git clone https://github.com/bidaian/no-systemd.git
-cd no-systemd
-sudo apt-get build-dep dbus gvfs network-manager policykit-1 udisks2
-apt-get source dbus gvfs network-manager policykit-1 udisks2
+sudo apt-get build-dep dbus network-manager policykit-1 udisks2
+apt-get source dbus network-manager policykit-1 udisks2
 
 cd dbus-1.10.22/
-patch -p2 < ../dbus/patch.txt
-gvfs-1.30.4/
-patch -p2 < ../gvfs/patch.txt
-network-manager-1.6.2/
-patch -p2 < ../network-manager/patch.txt
-policykit-1-0.105/
-patch -p2 < ../policykit-1/patch.txt
-udisks2-2.1.8/
-patch -p2 < ../udisks2/patch.txt
-
+patch -p2 < ../no-systemd/dbus/patch.txt
+dpkg-buildpackage -us -uc -nc
+cd ..
+cd network-manager-1.6.2/
+patch -p2 < ../no-systemd/network-manager/patch.txt
+dpkg-buildpackage -us -uc -nc
+cd ..
+cd policykit-1-0.105/
+patch -p2 < ../no-systemd/policykit-1/patch.txt
+dpkg-buildpackage -us -uc -nc
+cd ..
+cd udisks2-2.1.8/
+patch -p2 < ../no-systemd/udisks2/patch.txt
+dpkg-buildpackage -us -uc -nc
+cd ..
